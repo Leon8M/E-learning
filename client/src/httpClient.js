bcd13578085b140a0bc74b20b0fb0f11cb9a9144
Leon Munene <leonmunene@gmail.com>
2025-02-08 20:26:01 +0300

    more mre

--- client/src/httpClient.js
+++ client/src/httpClient.js
@@ -1,5 +1,23 @@
 import axios from "axios";
 
-export default axios.create({
-    withCredentials: true
-})
+const httpClient = axios.create({
+    baseURL: "https://e-learning-nvak.onrender.com",  // Ensure this is correct
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    }
+});
+
+// Request Interceptor (Attach token automatically)
+httpClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token"); // Ensure token is stored
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default httpClient;
