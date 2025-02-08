a39b97739fd70e0c35c2412a47e8f93b678656fd
Leon Munene <leonmunene@gmail.com>
2025-02-08 19:41:24 +0300

    redis

--- server/config.py
+++ server/config.py
@@ -16,4 +16,4 @@ class ApplicationConfig:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url("redis://red-cujogvrv2p9s7384kmig:6379")
